210c3e7775b2e38f0cd03674c3bfc6a8e1db8fdc
frozenlzt <549458594@qq.com>
2019-08-02 19:35:10 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+ulb_manager
---
+++ ulb_manager/wsgi.py
@@ -0,0 +1,24 @@
+"""
+WSGI config for ulb_manager project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+# import os
+#
+# from django.core.wsgi import get_wsgi_application
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ulb_manager.settings')
+#
+# application = get_wsgi_application()
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ulb_manager.prod")
+
+application = get_wsgi_application()
